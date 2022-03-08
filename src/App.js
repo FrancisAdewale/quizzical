6fbb3959dade3fa9c9d419b878b96974ce8a532f
Francis Adewale <francisadewale1992@gmail.com>
2022-03-08 16:40:17 +0000

    had issues with button toggling, used the complete wrong logic

--- src/App.js
+++ src/App.js
@@ -4,36 +4,71 @@ import Blob2 from "./imgs/blob2.png"
 import Intro from "./Intro"
 import Question from "./Question"
 import Submit from "./Submit"
+import { nanoid } from 'nanoid'
 
 export default function App() {
 
 const [startQuiz, setStartQuiz] = useState(false)
-const [questions, setQuestions] = useState([])
+const [questionsData, setQuestionsData] = useState([])
 
-//console.log(jsondata)
+const questionsArray = questionsData.map((prevData, index) => {
+  return {
+    question: prevData.question,
+    index: index,
+    correct_answer : prevData.correct_answer,
+    incorrect_answers : prevData.incorrect_answers,
+    id: nanoid()
+  }
+})
 
+const [buttonsArray, setButtonsArray] = questionsArray.map(item => {
+  return {
+    buttonsIndex : item.index,
+    correct: item.correct_answer,
+    incorrect: item.incorrect_answers,
+    isCorrect: false
+
+  }
+})
 
 
 useEffect(() => {
   fetch("https://opentdb.com/api.php?amount=5&category=21&difficulty=medium&type=multiple")
   .then(res => res.json())
-  .then(data => setQuestions(data.results))
+  .then(data => setQuestionsData(data.results))
 
-}, [startQuiz])
+}, [])
 
 
 function handleClick() {
   setStartQuiz(prevState => !prevState)
   }
 
-const questionsElement = questions.map(question => {
-  return <Question 
+  function selectAnswer(event) {
+
+    const {name, id} = event.target
+
+    console.log(id)
+
+  }
+
+
+const questionsElement = questionsArray.map(question => {
+
+  
+  return <Question
+  key={question.id} 
   question={question.question}
   correct_answer={question.correct_answer}
   incorrect_answerOne={question.incorrect_answers[0]}
   incorrect_answerTwo={question.incorrect_answers[1]}
-  incorrect_answerThree={question.incorrect_answers[2]} />
+  incorrect_answerThree={question.incorrect_answers[2]} 
+  index={question.index}
+  handleAnswer={selectAnswer}
+  />
 })
+
+
 
   return (
 
@@ -41,7 +76,7 @@ const questionsElement = questions.map(question => {
       <div className="middle">
         <img src={Blob1} className="blob1"/>
         <img src={Blob2} className="blob2"/>
-        { startQuiz ? questionsElement: <Intro handleClick={handleClick} />}
+        { startQuiz ? questionsElement : <Intro handleClick={handleClick} />}
         { startQuiz && < Submit />}
       </div>
 
--- src/Question.js
+++ src/Question.js
@@ -1,14 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Question(props) {
+    
+
+
+   
+
 
     return (
         <div className="question-container">
             <h2 className="question">{props.question}</h2>
-            <button className="choice">{props.correct_answer}</button>
-            <button className="choice">{props.incorrect_answerOne}</button>
-            <button className="choice">{props.incorrect_answerTwo}</button>
-            <button className="choice">{props.incorrect_answerThree}</button>
+            <button name="correct" id={props.index} className="choice" onClick={(e) => props.handleAnswer(e)}>{props.correct_answer}</button>
+            <button name="incorrectOne" className="choice">{props.incorrect_answerOne}</button>
+            <button name="incorrectTwo"className="choice"  >{props.incorrect_answerTwo}</button>
+            <button name="incorrectThree"className="choice" >{props.incorrect_answerThree}</button>
             <hr/>
             
         </div>
