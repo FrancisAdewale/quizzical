9d3db58ef7e97ec9f4a4b311a2e4de56c9042a01
Francis Adewale <francisadewale1992@gmail.com>
2022-03-13 15:42:36 +0000

    update h3 tag, also need to address synbols not rendering correctly, as well as users should only be able to select one button/answer

--- src/Intro.js
+++ src/Intro.js
@@ -5,7 +5,7 @@ export default function Intro(props) {
     return (
         <div className="inner">
         <h1>Quizzical</h1>
-        <h3>Some description if needed</h3>
+        <h3>Test your general knowledge!</h3>
         <button className="start-btn" onClick={props.handleClick}>Start quiz</button>
       </div>
     )
